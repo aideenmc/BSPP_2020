741b4de2500966ea049a95756931f2ffc616cd38
Aideen McCabe <aideen.mccabe@ucdconnect.ie>
2020-08-10 15:37:23 +0100

    Added comments

--- hotspot_analysis.R
+++ hotspot_analysis.R
@@ -1,13 +1,17 @@
 #Hub analysis
+#The purpose of this script is to get miRNA and TF interactions from hotspot genes
+#Also writes a fasta file of all wheat genes from IWGSC
+
+#importing relevant packages
 library(dplyr)
 library(readr)
 library(Biostrings)
 library(tidyr)
 
-#importing file
+#importing hotspot
 final_hotspots <- read_csv("Data/final_hotspots.csv")
 
-#extracting geneID to run through miRNA database
+#extracting geneIDs to run through miRNA database
 hotspot_id <- final_hotspots %>% 
   select(GeneID) %>% 
   unique()
@@ -15,14 +19,18 @@ hotspot_id <- final_hotspots %>%
 #write to file
 write.table(hotspot_id, "hotspot_id.txt", quote = FALSE, row.names = FALSE)
 
+#geneIDs were analysed for miRNA interaction partners (python notebook)
 #this returns a csv file with miRNA interactions from PmiREN database
 hotspot_miRNA <- read_csv("hotspot_miRNA.csv")
 
 #now to get TFs
 #first need sequence of genes
-#fasta_hotspot <- read.fasta("/Users/aideenmccabe/Documents/College Documents/BSPP2020/BSPP_2020/Data/wheat_all.fa")
+
+#read in all annotated genes from IWGSC as a DNAStringSet
 fasta_hotspot <- readDNAStringSet("/Users/aideenmccabe/Documents/College Documents/BSPP2020/BSPP_2020/Data/wheat_all.fa")
+#convert to dataframe
 fastadf<-as.data.frame(fasta_hotspot)
+#formatting
 fastadf$names <- rownames(fastadf)
 rownames(fastadf) <- NULL
 colnames(fastadf) <- c("sequence", "info") 
@@ -39,12 +47,12 @@ write.csv(full_fasta, "full_fasta.csv", row.names = FALSE)
 #cut off the trailing number
 full_fasta$geneID = substr(full_fasta$geneID, 1, nchar(full_fasta$geneID)-2)
 
-#crossover
+#getting hotspot sequences
 crossover <-full_fasta %>% 
   filter(geneID %in% hotspot_id$GeneID) %>% 
   distinct(geneID, .keep_all = TRUE)
 
-#reorder
+#reordering columns
 to_fasta <- crossover[, c(2, 1)]
 
 #now to convert to fasta file
