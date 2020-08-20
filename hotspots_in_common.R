398592d8b5580394ffe844b99fca9acb41aeaaf5
Aideen McCabe <aideen.mccabe@ucdconnect.ie>
2020-08-20 13:54:07 +0100

    improved function: converts dataframes into lists of hotspots compares hotspots and if >80% similar, appends these genes to a list concatanates all results into one dataframe with column specifying which dataset

--- hotspots_in_common.R
+++ hotspots_in_common.R
@@ -1,22 +1,50 @@
 library(dplyr)
 library(readr)
 
+#importing example file
+final_hotspots <- read_csv("Data/final_hotspots.csv")
+final_hotspots$Hotspot <- sub("^", "Fusarium", final_hotspots$Hotspot)
 
-matchy <- function(hotspot1, hotspot2){
+#split dataset into a list of smaller dfs based on hotspot
+dataset1 <- split(final_hotspots , f = final_hotspots$Hotspot)
+
+dataset2 <- split(final_hotspots , f = final_hotspots$Hotspot)
+
+
+#######this works!
+matchy <- function(out, out2){
   
-  max <- if (length(hotspot1) > length(hotspot2)) hotspot1 else hotspot2
-  min <- if (length(hotspot1) < length(hotspot2)) hotspot1 else hotspot2
-  crosscheck <- max$GeneID %in% min$GeneID
-  match <- if (length(crosscheck[crosscheck==TRUE]) > length(min)*0.8) TRUE else FALSE
+  matches <- list()
   
-  if (match == TRUE) {
-    return(as.data.frame(bind_rows(max, min)))
+  for (i in 1:length(out)){
+  
+    for (j in 1:length(out2)) {
+        max <- if (nrow(out[[i]]) > nrow(out2[[j]])) out[[i]] else if (nrow(out[[i]]) == nrow(out2[[j]])) out[[i]] else out2[[j]]
+        min <- if (nrow(out[[i]]) < nrow(out2[[j]])) out[[i]] else if (nrow(out[[i]]) == nrow(out2[[j]])) out2[[j]] else out2[[j]]
+        crosscheck <- max %>% 
+          filter(GeneID %in% min$GeneID)
+        match <- if (nrow(crosscheck) > nrow(min)*0.8) TRUE else FALSE
+        
+        if (match == TRUE){
+          #need to make sure name of hotspot in original inputs contains name of dataset
+          crosscheck$comparison <- paste(out[[i]][[1]][[1]], out2[[j]][[1]][[1]], sep = "_")
+          matches[[j]] <- as.data.frame(crosscheck)
+          print(match)
+        }
+        
+        else
+          print(match)
+      }
+  
   }
-  else {
-    return()
-  }
-  
+
+  return(matches)
 }
 
-test <- matchy(final_hotspots, hotspots2)
+#running the function and turning the list into one big dataframe
+test <- matchy(dataset1, dataset2)
+testdf <- bind_rows(test, .id = "column_label")
+
+#need to expand this to take more than two datasets as input 
+
 
